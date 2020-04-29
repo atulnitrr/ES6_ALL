134c8d00a182b10eb60c2c4310966c805795af88
Atul Kumar <atul.kumar3@go-mmt.com>
2020-04-29 10:08:04 +0530

    Strict mode

---
+++ basic/basic.js
@@ -0,0 +1,38 @@
+console.log("Basic ");
+let varObject = {
+  name: "atul",
+  greet(data) {
+    console.log(`Data --> ${this.name} ${data}`);
+    return `Data --> ${this.name}`;
+  },
+};
+console.log(varObject.__proto__);
+varObject.greet();
+const gt = varObject.greet;
+// gt.call(varObject, [2, 3]);
+// gt.apply(varObject, [2, 3]);
+
+console.log(varObject);
+console.log(typeof varObject);
+/*
+const data = [1, 2, 3];
+console.log(data[7]);
+
+const var1 = undefined;
+console.log(var1);
+
+const varn = null;
+console.log(varn);
+
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+
+const varnumber = NaN;
+console.log(varnumber);
+console.log(typeof NaN);
+
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+*/
---
+++ basic/basicstrrict.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("strict mode");
+const a = 12;
+console.log(a);
