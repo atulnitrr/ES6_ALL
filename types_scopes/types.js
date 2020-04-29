4dbac8d2c0e2ce6862c0bb58d5db875df3b36901
Atul Kumar <atul.kumar3@go-mmt.com>
2020-04-29 17:37:06 +0530

    Global vs local scope

---
+++ types_scopes/types.js
@@ -0,0 +1,37 @@
+/*
+  primtiive type , nubmeer bool , string
+*/
+
+var test = "test outsidee ";
+
+{
+  var test = "test inside";
+  console.log(test);
+}
+
+console.log(test);
+
+/*
+
+let test = "test outsidee ";
+
+{
+  let test = "test inside";
+  console.log(test);
+}
+
+console.log(test);
+
+*/
+
+/*
+var a = 7;
+var b = a;
+b = 10;
+console.log(a);
+console.log(b);
+*/
+// "use strict";
+// var var1 = 10;
+// eval("var cc = 12");
+// console.log(var1);
