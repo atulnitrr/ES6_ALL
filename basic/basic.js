a8b42661a472f9834b0f099454dced8ac44225d6
Atul Kumar <atul.kumar3@go-mmt.com>
2020-04-29 15:35:52 +0530

    Type and scopes

--- basic/basic.js
+++ basic/basic.js
@@ -1,5 +1,126 @@
 console.log("Basic ");
 
+var a = 5;
+var b = 6;
+
+console.log(a + b * 2);
+/*
+
+if (1 == 1 && 3 == 2) {
+  console.log("true");
+} else {
+  console.log("false ");
+}
+
+*/
+/*
+
+console.log(1 == 1);
+console.log(0 == undefined);
+console.log(null == undefined);
+console.log(NaN == NaN);
+console.log(NaN != NaN);
+console.log(0 == null); // false --> null cant be compared
+
+var a = 12;
+var b = 2;
+console.log(a / b);
+
+var a = 12;
+var b = "2";
+console.log(a / b);
+
+var a = "12";
+var b = "2";
+console.log(a / b);
+
+var a = "12";
+var b = "stt";
+console.log(a / b);
+
+var a = 3.3;
+var b = 2.2;
+console.log(a / b);
+
+var a = 10;
+var b = 3;
+console.log(a % b);
+
+var a = 10;
+var b = 0;
+console.log(a / b);
+
+*/
+/*
+var a = 12;
+var b = 10;
+console.log(a * b);
+
+var a = 1.2;
+var b = 10;
+console.log(a * b);
+
+var a = 1.3;
+var b = 2.2;
+console.log(a * b);
+console.log((a * b).toFixed(2));
+
+var a = 2;
+var b = "2.2";
+console.log(a * b);
+
+var a = "st";
+var b = "2.2";
+console.log(a * b);
+
+var a = 15;
+var b = Infinity;
+console.log(a * b);
+
+*/
+/*
+var a = 12;
+var b = 10;
+console.log(a - b);
+
+var a = "ss";
+var b = "s";
+console.log(a - b);
+
+var a = 12;
+var b = "1";
+console.log(a - b);
+
+var a = "13";
+var b = 1;
+console.log(a - b);
+*/
+
+//operators
+
+// var a = true;
+// var b = true;
+// console.log(a + b);
+
+// var a = NaN;
+// var b = true;
+// console.log(a + b);
+
+// var a = null;
+// var b = true;
+// console.log(a + b);
+
+// var a = "Join ";
+// var b = 10.2;
+// console.log(a + b);
+
+// var a = 5.3;
+// var b = 10.2;
+// console.log(a + b);
+// var a = 5;
+// var b = 10;
+// console.log(a + b);
+
 /*
 let number = 9;
 
