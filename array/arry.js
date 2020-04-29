4d5a96207448e8e1674516a88167e14061c0dbb5
Atul Kumar <atul.kumar3@go-mmt.com>
2020-04-29 19:06:07 +0530

    Added arra

---
+++ array/arry.js
@@ -0,0 +1,75 @@
+console.log("array js");
+
+var array = [1, 2, 3, 4];
+var tt = array.reduce((total, value) => {
+  return total + value;
+}, 12);
+console.log(tt);
+/*
+var array = [1, 2, 3, 5];
+var newArray = ["join", "me", "tt"];
+
+console.log(array.concat(newArray));
+console.log(array.join(newArray));
+
+console.log(array);
+console.log(newArray);
+
+*/
+
+/*
+var array = [1, 2, 3, 4, 5];
+
+console.log(array.filter((value) => value > 2));
+console.log(array.map((value) => value * 3));
+console.log(array.reverse());
+console.log(array);
+
+*/
+
+/*
+var newArray = array.slice(1, 3);
+console.log(array);
+console.log(newArray);
+
+*/
+
+/*
+console.log(array);
+var newArray = array.splice(1, 2);
+console.log(newArray);
+console.log(array);
+
+*/
+
+/*
+array.unshift("new");
+console.log(array.indexOf("new"));
+console.log(array.indexOf(1));
+
+*/
+
+// array.push(5);
+// array.pop();
+// array.shift();
+// array.unshift(8);
+// console.log(array);
+/*
+array.forEach((elemt, index) => console.log(elemt, index));
+
+*/
+
+/*
+array[6] = 100;
+console.log(array);
+console.log(array.length);
+console.log(array[2]);
+
+*/
+
+/*
+var array = [1, 2, 3];
+console.log(array.length);
+console.log(array[2]);
+
+*/
