b5ca29b5d5508d6a38558dcd1ff54d8d5ab21688
Atul Kumar <atul.kumar3@go-mmt.com>
2020-04-30 08:58:58 +0530

    error handeling

---
+++ erro_handeling/eerrors.js
@@ -0,0 +1,22 @@
+console.log("errors js");
+
+try {
+  test();
+} catch (error) {
+  console.log(error);
+} finally {
+  console.log("finally  done");
+}
+
+/*
+var a = 3;
+var b = 5;
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+  a = i;
+}
+console.log(a);
+console.log(b);
+
+*/
