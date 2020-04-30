d69e151a2b5204bcc5857b900ea0d020769972cd
Atul Kumar <atul.kumar3@go-mmt.com>
2020-04-30 08:10:56 +0530

    Added call apply and bind

--- objects/object.js
+++ objects/object.js
@@ -2,10 +2,128 @@ console.log("Object js");
 /*
 No of ways to create java script object
 1. Object literal 
-2. Object constructor. 
+2. Object constructor.   new Object();
 3. Object.create() --> it set prototype to Object
+4. Constructor function --> 
+5. Bind you are binding this value and may call later 
 */
 
+/*
+var person = {
+  name: "autl",
+  greettt() {
+    console.log(this);
+  },
+
+  greetFat: () => {
+    console.log(this);
+  },
+};
+
+person.greettt();
+person.greetFat();
+// console.log(person);
+*/
+/*
+var person = {
+  name: "Atul",
+  greet: function (message1, message2) {
+    console.log(this);
+    console.log(`Hello ${this.name} ${message1} ${message2}`);
+  },
+};
+
+// person.greet("hello", "dama");
+let gt = person.greet;
+// gt("hello", "dama");
+// gt.bind(person, "hello", "dama")();
+// gt.call(person, "hello", "dama");
+gt.apply(person, ["hello", "dama"]);
+*/
+/*
+function fn(message, message2) {
+  console.log(message, message2, this);
+}
+
+var obj = {
+  fun: fn,
+};
+var person = {
+  name: "atul",
+};
+
+obj.fun.apply(person, ["call1"]);
+// obj.fun.call(person, "call", "call2");
+// obj.fun.bind(person, "tada", "kk")();
+// obj.fun("dd");
+*/
+
+/*
+function fn() {
+  console.log(this);
+}
+
+var obj = {
+  name: "atul",
+  fun: fn,
+};
+
+// obj.fun(); // it will revfer obj
+// obj.fun.bind(this)();
+
+var person = {
+  name: "atul",
+};
+
+obj.fun.bind(person)();
+*/
+/*
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+var person1 = new Person("atul", 56);
+var person2 = new Person("amit", 66);
+console.log(person1, person2);
+*/
+/*
+
+function Person() {
+  this.name = "atul";
+  this.age = 34;
+  this.greet = function () {
+    console.log(`Hello in am ${this.name} age ${this.age}`);
+  };
+}
+
+Person.prototype.greetGeneral = function () {
+  console.log("Hello general");
+};
+
+var person = new Person();
+person.name = "ana";
+person.greet();
+person.greetGeneral();
+var anotherPerson = new Person();
+anotherPerson.greet();
+anotherPerson.greetGeneral();
+
+console.log(person instanceof Person);
+*/
+/*
+function Person() {}
+Person.prototype.greet = function () {
+  console.log("Data");
+};
+
+var person = new Person();
+person.greet();
+console.log(person);
+console.log(Person.prototype);
+console.log(Object.getPrototypeOf(person));
+console.log(Object.getPrototypeOf(person) == Person.prototype);
+*/
 /*
 var person = {
   name: "atul",
