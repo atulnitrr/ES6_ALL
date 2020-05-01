3736abc7f535f2c7cf4d41e30ac1bff594e86a0f
Atul Kumar <atul.kumar3@go-mmt.com>
2020-05-01 09:49:24 +0530

    built in objects

---
+++ buildt_in_obj/built_in.js
@@ -0,0 +1,87 @@
+console.log("Built in");
+/**
+ * 1. Timers and interval --> SetTimeout and INterval
+ * 2.Format and value
+ * 3. String format
+ * 4. The math object
+ * 5. The date object
+ * 6. Regular expression
+ *
+ */
+
+/*
+var string = "acab";
+var pattern = /ab/;
+console.log(pattern.exec(string));
+console.log(pattern.test(string));
+console.log(string.match(pattern));
+*/
+/*
+var data = new Date("2020/06/12");
+console.log(data);
+console.log(Date.parse("2020/06/12"));
+console.log(new Date(1591900200000));
+*/
+/*
+var today = new Date();
+console.log(today);
+
+var dd = new Date(2020, 6, 12);
+console.log(dd);
+*/
+
+/*
+console.log(Math.max(3, 4, 55, 6));
+var varr = 1.26;
+console.log(Math.round(varr));
+var pi = Math.PI;
+console.log(pi);
+console.log(Math.E);
+console.log(Math.abs(-3));
+*/
+
+/*
+var ss = "aa bb cc";
+var tt = " bb cc ";
+
+console.log(tt.trim());
+console.log(tt);
+console.log(ss.split(" "));
+console.log(ss);
+console.log(ss.toUpperCase());
+console.log(ss);
+console.log(ss.concat(" dd"));
+console.log(ss);
+console.log(ss.charAt(3));
+console.log(ss[1]);
+console.log(ss.length);
+console.log(ss);
+
+*/
+
+// var a = "1";
+
+/*
+var a = 10;
+console.log(a);
+console.log(a.toString());
+var a = "jjj";
+var a = "ff445";
+console.log(parseInt(a, 16));
+console.log(a);
+console.log(parseInt(a));
+*/
+/*
+setTimeout(function () {
+  console.log("Done-->");
+}, 2000);
+
+var inter = setInterval(() => {
+  console.log("Pint");
+}, 1000);
+
+setTimeout(() => {
+  clearInterval(inter);
+}, 5000);
+
+*/
--- function/function.js
+++ function/function.js
@@ -1,8 +1,42 @@
 console.log("function js");
 /**
  1. Closuures 
+ 2. IIFE --> why need this, local scope dont mess with global variable
+ 3. Built in methods 
  */
 
+function fund(mess, numbers) {
+  console.log(mess);
+  console.log(arguments);
+}
+
+fund("aaarr", 34);
+
+console.log(fund.length); // no of argument epect
+
+console.log(fund.name);
+
+var msg = function (message) {
+  console.log(message);
+};
+console.log(msg.name);
+
+function fun2() {
+  console.log(this.name);
+}
+
+fun2();
+/*
+function fun() {
+  var dd = 90;
+}
+
+((a) => {
+  console.log(a);
+})(23);
+*/
+
+/*
 function fund(input) {
   return function () {
     return input * 2;
@@ -13,3 +47,5 @@ var data = fund(100)();
 var data2 = fund(300)();
 console.log(data);
 console.log(data2);
+
+*/
